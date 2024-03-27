6ba23d1e3d815cf26f10f20e00d4117bffa1861b
DrFilmBuzz <wesley_gift@yahoo.com>
2024-03-27 18:23:03 -0400

    homework attempt 1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
-
+# Fizzbuzz printing numbers 1 to 100
+for num in range (1, 101):
+    print(num)
+    if num % 3 == 0 and num % 5 == 0:
+        print("Fizzbuzz")
+        continue
+    elif num % 3 == 0:
+        print("Fizz")
+        continue
+    elif num % 5 == 0:
+        print("Buzz")
+        continue
+    
